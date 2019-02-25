dc00419fa680cfc5e0c1d438c88db6942e3f3d6a
Lukas Pospisil <lurttik@gmail.com>
2019-02-25 14:50:59 +0100

    show edit icon only for non-complete notes

--- components/NoteItem.js
+++ components/NoteItem.js
@@ -74,17 +74,20 @@ const NoteItem = ({ id, title, finished }) => {
             onBlur={() => handleOnBlur(updateNote)}
           />
           <Icons>
-            <Button type="button" onClick={handleFocus} disabled={finished}>
-              <svg
-                className="icon-edit"
-                height="24"
-                viewBox="0 0 24 24"
-                width="24"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path d="M3 17.25V21h3.75L17.81 9.94l-3.75-3.75L3 17.25zM20.71 7.04c.39-.39.39-1.02 0-1.41l-2.34-2.34c-.39-.39-1.02-.39-1.41 0l-1.83 1.83 3.75 3.75 1.83-1.83z" />
-              </svg>
-            </Button>
+            {!checked && (
+              <Button type="button" onClick={handleFocus} disabled={finished}>
+                <svg
+                  className="icon-edit"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  width="24"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path d="M3 17.25V21h3.75L17.81 9.94l-3.75-3.75L3 17.25zM20.71 7.04c.39-.39.39-1.02 0-1.41l-2.34-2.34c-.39-.39-1.02-.39-1.41 0l-1.83 1.83 3.75 3.75 1.83-1.83z" />
+                </svg>
+              </Button>
+            )}
+
             <DeleteNote id={id} />
           </Icons>
         </Note>
